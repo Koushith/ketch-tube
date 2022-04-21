271e8f782c842045471fcbd42844411619f2f565
koushith <koushith97@gmail.com>
2022-04-22 02:46:05 +0530

    adds initial data

---
+++ src/backend/db/videos.js
@@ -0,0 +1,191 @@
+/**
+ * Videos Database can be added here.
+ * You can add videos of your wish with different attributes
+ * */
+
+export const videos = [
+  {
+    _id: 'BnHwoSwoqAQ',
+    title: 'Fashion week 2015 VOGUE Aftermovie',
+    description: 'to be updated',
+    creator: 'CONTENT FACTORY',
+    category: 'Self Help',
+    uploaded: 'Feb 19, 2016',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment: 'Loved the work. stunning cinemotography',
+        color: 'hotpink',
+      },
+      {
+        user: 'Roma Bulani',
+        comment: 'Loved this video. A brilliantly visual and fun-to-watch',
+        color: 'brown',
+      },
+    ],
+  },
+  {
+    _id: 'Xp1Yvg6Du_w',
+    title: 'BCEFW SS21 Digital Fashion Weeks official aftermovie',
+    description:
+      'Yo Yo Yo! Make sure you suggest some of your favourite books to me too! Lemme kno you liked the books from this video! LoveLove!',
+    creator: 'Budapest Central European Fashion Week',
+    category: 'Others',
+    uploaded: 'Jan 12 2021',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment:
+          "The diary of a young girl- by Anne Frank... I've read it in class 10 not by my choice but we're forced to",
+        color: 'hotpink',
+      },
+      {
+        user: 'Roma Bulani',
+        comment: 'The Alchemist is a great book for a short read. Love your choices!',
+        color: 'brown',
+      },
+    ],
+  },
+  {
+    _id: 'LL7x5zxTFX8',
+    title: 'Prasad Bidapa Fashion Week 2013 - After Movie',
+    description: 'A Promo Film by GOLD CHASE MOTION PICTURES & CENTERSTAGE Directed by VISHAL KHIMRAJ',
+    creator: 'Action Amaze',
+    category: 'Non Fiction',
+    uploaded: 'May 1 2020',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment:
+          "Funny to see you talk about this book! The author is from my region and I've been reading his books since I was 13... glad to see he has readers all over the world now!",
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'Exn5BlubYIw',
+    title: 'Indonesia Fashion Week - The Aftermovie 2019',
+    creator: 'Athea Visuals',
+    description: 'lorem ipsm',
+    category: 'Others',
+    uploaded: 'Apr 1 2022',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment: 'Most of those are in my top list too :) Happy Reading to.all',
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'vYtADou9xuM',
+    title: 'Lakme Fashion Week SR',
+    description:
+      "Another season of elegance, innovation, and style at the Lakme Fashion Week SR'19 driven by NEXA, saw the biggest names in the fashion and lifestyle segment display their love and support for the industry. Watch this video for a quick view of all that happened in those five days of fashion.",
+    creator: 'Helly',
+    category: 'Fiction',
+    uploaded: 'Mar 12 2018',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment: "yesterday I've completed it",
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: '7QEuApp7Cfk',
+    title: 'ENTRAL MR.& MS. GUJARAT OFFICIAL',
+    description: 'Vadodara Spring Summer Fashion Showcase 2017 Presents by:- Central',
+    creator: 'Mostly Sane',
+    category: 'Others',
+    uploaded: 'Feb 12 2015',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment: 'Hello Ankur sir, I would love to read a book that taught you courage!',
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'YL6N659uc38',
+    title: 'KIDS FASHION SHOW ',
+    description: 'Kids Fashion Dress. Designer Julia Radova. Belarus Fashion Week. Season SS2018.',
+    creator: 'Fashion Channel Belarus',
+    category: 'Self Help',
+    uploaded: 'Sep 13 2019',
+    comments: [
+      {
+        user: 'Fashion Channel Belarus',
+        comment: 'I will go through this video once more & will read this book also someday. Thanks Warikoo!',
+        color: 'hotpink',
+      },
+    ],
+  },
+
+  {
+    _id: '5nKlU0i1mMM',
+    title: 'Rachna Soni | Chennai Show | IKFW season 8 | Indias Kids Fashion Week | IKFW2021',
+    description:
+      "We are in the Fashion industry for 'Kids'. It is a foremost initiative to give India's upcoming and highly potential fashion fraternity for kids a platform to showcase their talent and make a mark in the international fashion industry. It will be a 360 degree event for the kids industry, where brands ",
+    creator: 'India Kids Fashion Week',
+    category: 'Others',
+    uploaded: 'Nov 12 2021',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment: 'Good encouragement show 👍',
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'nUizoL723bE',
+    title: 'Little Secrets at IKFW MUMBAI - India Kids Fashion Week Season 6',
+    description: 'India Kids Fashion Week is a signature affair to position India on the map of fashion industry',
+    creator: 'Book Junction',
+    category: 'Non Fiction',
+    uploaded: 'Dec 15 2021',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment:
+          'I was also skeptical like you whether I should go for this book or not now I will definitely go for it. ',
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'nUizoL723bE',
+    title: 'Little Secrets at IKFW MUMBAI - India Kids Fashion Week Season 6',
+    description: 'India Kids Fashion Week is a signature affair to position India on the map of fashion industry',
+    creator: 'Book Junction',
+    category: 'Non Fiction',
+    uploaded: 'Dec 15 2021',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment:
+          'I was also skeptical like you whether I should go for this book or not now I will definitely go for it. ',
+        color: 'hotpink',
+      },
+    ],
+  },
+  {
+    _id: 'nUizoL723bE',
+    title: 'Little Secrets at IKFW MUMBAI - India Kids Fashion Week Season 6',
+    description: 'India Kids Fashion Week is a signature affair to position India on the map of fashion industry',
+    creator: 'Book Junction',
+    category: 'Non Fiction',
+    uploaded: 'Dec 15 2021',
+    comments: [
+      {
+        user: 'Bablu Trailor',
+        comment:
+          'I was also skeptical like you whether I should go for this book or not now I will definitely go for it. ',
+        color: 'hotpink',
+      },
+    ],
+  },
+];
